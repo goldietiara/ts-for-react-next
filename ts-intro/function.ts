c48cc0a34d35145678880faaf023b1ac3d529465
Goldie Tiara <goldietiara.acc@gmail.com>
2023-06-30 10:02:59 +0700

    ts function

---
+++ ts-intro/function.ts
@@ -0,0 +1,36 @@
+//FUNCTION
+
+let hello = () => {
+  console.log("hello");
+  return console.log("hello");
+  // return "hello"
+  // return is void
+};
+
+let funcStr = (): string => {
+  // console.log("hi")
+  return "hello everynyan";
+};
+
+let multiple = (num: number) => {
+  return num * 2;
+};
+let multiple2 = (num: number): number => {
+  return num * 2;
+};
+// funciton multiple and multiple2 is the same
+
+let multiple3 = (num: number): void => {
+  //   return num * 2;
+  //  cant return num because the return is void
+  return console.log(num * 2);
+};
+
+let sum = (num: number, num2: number, num3?: number) => {
+  return num + num2;
+};
+console.log(sum(5, 5));
+
+let user = (username: { name: string; age: number }) => {
+  console.log(username.name);
+};
